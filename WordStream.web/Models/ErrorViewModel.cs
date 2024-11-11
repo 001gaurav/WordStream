766b2f7940d00c9b40aca8751f3ba52dab042ceb
001gaurav <ahiregaurav3122@gmail.com>
2024-11-11 17:46:43 +0530

    Add project files.

---
+++ WordStream.web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WordStream.web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
